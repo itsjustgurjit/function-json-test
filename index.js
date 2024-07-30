807c7177ca1c058fe5943fc7ad22a72b83839efb
Gurjit Singh <gurjitsinghisere@gmail.com>
2024-07-30 10:51:42 +0530

    multiply added

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/posts', (req, res) => {
   res.json(data);
 });
 
+app.get('/multiply/:number', (req, res) => {
+    const number = parseInt(req.params.number);
+    console.log(number);
+    const result = number * 5 +' hello';
+    fs.appendFileSync('data.json', JSON.stringify(result) + ',\n');
+    res.json({ result , req});
+  });
+  
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port', process.env.PORT || 3000);
 });
